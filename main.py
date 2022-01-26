a07f7cfb8b6f45bd57712372823fcd8f53dee1cb
ahmedsaad0885 <83538365+ahmedsaad0885@users.noreply.github.com>
2022-01-26 14:06:16 +0200

    guassjordan and guassian elemination and tried GUI

--- main.py
+++ main.py
@@ -19,14 +19,30 @@ lbl2.grid(column=0, row=2)
 
 lbl3 = Label(window, text="f(x3)", font=("Arial Bold ", 20))
 lbl3.grid(column=0, row=3)
+
+reslbl = Label(window, text="", font=("Arial Bold ", 20))
+reslbl.grid(column=0, row=5)
 #Entries
 
-entry1=Entry(window, width=10)
-entry1.grid(column=1,row=1)
+filename=Entry(window, width=20)
+filename.grid(column=1, row=0)
+func1=Entry(window, width=20)
+func1.grid(column=1, row=1)
+func2=Entry(window, width=20)
+func2.grid(column=1, row=2)
+func3=Entry(window, width=20)
+func3.grid(column=1, row=3)
+
 #functions
 
 
+def addnum():
+
+
+    print(tempfunc1)
+    lbl1.configure(text=tempfunc1)
 def gauss_elimination():
+
     i = 0
     x = [0] * 3
     coffmat = []
@@ -36,7 +52,17 @@ def gauss_elimination():
             col.append(0)
         coffmat.append(col)
 
-    all_equations = ["2x+1y+4z+1", "1x+2Y+3z+1.5", "2-1y+4x+2z"]
+    all_equations = [0]*3
+    if readfile==True:
+        all_equations[0] = tempfunc1
+        all_equations[1] = tempfunc2
+        all_equations[2] = tempfunc3
+    else:
+        all_equations[0]=func1.get()
+        all_equations[1] = func2.get()
+        all_equations[2] = func3.get()
+    print(all_equations)
+    #all_equations = ["2x+1y+4z+1", "1x+2Y+3z+1.5", "2-1y+4x+2z"]
 
     def CoefficientIntercept(equation):
         if (equation.__contains__("x")) | (equation.__contains__("X")):
@@ -53,6 +79,8 @@ def gauss_elimination():
         else:
             coef_z = '0'
         intercept = re.sub("[+-]?\d+[XxYyZz]|[+-]?\d+\.\d+[XxYyZz]|[+-]?\d+\.\d+[XxYyZz]", "", equation)
+        if intercept =='':
+            intercept='0'
         return float(coef_x), float(coef_y), float(coef_z), float(intercept)
 
     i = 0
@@ -64,8 +92,11 @@ def gauss_elimination():
         coffmat[i][3] = cof_c
         # print(coffmat, flush=True)
         i = i + 1
-
+    print(coffmat)
     for b in range(3):
+        for m in range(4):
+            if coffmat[b][m] == -0:
+                coffmat[b][m] = 0
         for l in range(b + 1, 3):
             factor = coffmat[l][b] / coffmat[b][b]
             for c in range(b, 3):
@@ -79,7 +110,14 @@ def gauss_elimination():
         for s in range(q + 1, 3):
             sum = sum + coffmat[q][s] * x[s]
         x[q] = ((coffmat[q][3]) - sum) / coffmat[q][q]
+        if x[q]==-0:
+            x[q]=0
     print(x)
+    temp="x=: "+str(x[0])+"  y=: "+str(x[1])+"  z=: "+str(x[2])
+
+
+    reslbl.configure(text=temp)
+
 
 
 
@@ -93,10 +131,16 @@ def gauss_jordan():
         for j in range(4):
             col.append(0)
         coffmat.append(col)
+    all_equations = [0] * 3
+    if readfile==True:
+        all_equations[0] = tempfunc1
+        all_equations[1] = tempfunc2
+        all_equations[2] = tempfunc3
+    else:
+        all_equations[0] = func1.get()
+        all_equations[1] = func2.get()
+        all_equations[2] = func3.get()
 
-
-
-    all_equations = ["2x+1y+4z+1", "1x+2Y+3z+1.5", "2-1y+4x+2z"]
 
     def CoefficientIntercept(equation):
         if (equation.__contains__("x")) | (equation.__contains__("X")):
@@ -147,22 +191,108 @@ def gauss_jordan():
             for e in range(2, 4):
                 coffmat[0][e] -= factor1 * coffmat[2][e]
                 coffmat[1][e] -= factor2 * coffmat[2][e]
-        print(coffmat)
+        temp = "x=: " + str(coffmat[0][3]) + "  y=: " + str(coffmat[1][3]) + "  z=: " + str(coffmat[2][3])
 
-    print(coffmat)
+        reslbl.configure(text=temp)
 
 
 
-def addnum():
 
-    num=int(entry1.get())
-    num=num+5
-    lbl1.configure(text=str(num))
+
+
+def gauss_sidel():
+    i = 0
+    x = [0] * 3
+    coffmat = []
+    for i in range(3):
+        col = []
+        for j in range(4):
+            col.append(0)
+        coffmat.append(col)
+
+    all_equations = [0] * 3
+    if readfile == True:
+        all_equations[0] = tempfunc1
+        all_equations[1] = tempfunc2
+        all_equations[2] = tempfunc3
+    else:
+        all_equations[0] = func1.get()
+        all_equations[1] = func2.get()
+        all_equations[2] = func3.get()
+    print(all_equations)
+    #all_equations = ["2x+1y+4z+1", "1x+2Y+3z+1.5", "2-1y+4x+2z"]
+
+    def CoefficientIntercept(equation):
+        if (equation.__contains__("x")) | (equation.__contains__("X")):
+            coef_x = re.findall(r'-?[0-9.]*[Xx]', equation)[0][:-1]
+
+        else:
+            coef_x = '0'
+        if (equation.__contains__("y")) | (equation.__contains__("Y")):
+            coef_y = re.findall(r'-?[0-9.]*[Yy]', equation)[0][:-1]
+        else:
+            coef_y = '0'
+        if (equation.__contains__("z")) | (equation.__contains__("Z")):
+            coef_z = re.findall(r'-?[0-9.]*[Zz]', equation)[0][:-1]
+        else:
+            coef_z = '0'
+        intercept = re.sub("[+-]?\d+[XxYyZz]|[+-]?\d+\.\d+[XxYyZz]|[+-]?\d+\.\d+[XxYyZz]", "", equation)
+        if intercept =='':
+            intercept='0'
+        return float(coef_x), float(coef_y), float(coef_z), float(intercept)
+
+    i = 0
+    for equation in all_equations:
+        cof_x, cof_y, cof_z, cof_c, = CoefficientIntercept(equation)
+        coffmat[i][0] = cof_x
+        coffmat[i][1] = cof_y
+        coffmat[i][2] = cof_z
+        coffmat[i][3] = cof_c
+        # print(coffmat, flush=True)
+        i = i + 1
+
+    x[0]*3
+
+
+def read_from_file():
+    choice = filename.get()
+    if len(choice) != 0:
+        global readfile
+        readfile=True
+        file = open(choice, 'r')
+        n = (file.readline())
+        method = (file.readline())
+        global tempfunc1
+        global tempfunc2
+        global tempfunc3
+        tempfunc1= (file.readline())
+        tempfunc2 = (file.readline())
+        tempfunc3 = (file.readline())
+        if method=="Gauss-sidel":
+            global initial
+            initial = (file.readline())
+        if method == "Gaussian-elemination\n":
+            gauss_elimination()
+        if method == "Gaussian-jordan\n":
+            gauss_jordan()
+        #
+        # initial_guess = float(initial_guess)
+        # n = int(n)
+        # tol = float(tol)
+
+
 # button
 
 
 
-btn1 = Button(window, text="button",bg="#7322c7",fg="#f0f0f0",command =gauss_elimination)
+btn1 = Button(window, text="Gaussian-elimination",bg="#7322c7",fg="#f0f0f0",command =read_from_file)
+btn2 = Button(window, text="Gauss-jordan",bg="#7322c7",fg="#f0f0f0",command =read_from_file)
+btn3 = Button(window, text="Gauss-sidel",bg="#7322c7",fg="#f0f0f0",command =read_from_file)
+btn4 = Button(window, text="read from file",bg="#7322c7",fg="#f0f0f0",command =read_from_file)
+
 btn1.grid(column=1, row=4)
+btn2.grid(column=1, row=5)
+btn3.grid(column=1, row=6)
+btn4.grid(column=1, row=7)
 
 window.mainloop()

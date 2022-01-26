2d2613d8437140811b240aaac0e3cd7e38d35a49
ahmedsaad0885 <83538365+ahmedsaad0885@users.noreply.github.com>
2022-01-26 02:53:45 +0200

    guassjordan and guassian elemination and tried GUI

--- main.py
+++ main.py
@@ -8,13 +8,80 @@ window.geometry('600x600')
 # widgets
 
 # label
-lbl1 = Label(window, text="hello", font=("Arial Bold ", 20))
-lbl1.grid(column=0, row=0)
+lbl0 = Label(window, text="file name", font=("Arial Bold ", 20))
+lbl0.grid(column=0, row=0)
+
+lbl1 = Label(window, text="f(x1)", font=("Arial Bold ", 20))
+lbl1.grid(column=0, row=1)
+
+lbl2 = Label(window, text="f(x2)", font=("Arial Bold ", 20))
+lbl2.grid(column=0, row=2)
+
+lbl3 = Label(window, text="f(x3)", font=("Arial Bold ", 20))
+lbl3.grid(column=0, row=3)
 #Entries
 
 entry1=Entry(window, width=10)
-entry1.grid(column=0,row=1)
+entry1.grid(column=1,row=1)
 #functions
+
+
+def gauss_elimination():
+    i = 0
+    x = [0] * 3
+    coffmat = []
+    for i in range(3):
+        col = []
+        for j in range(4):
+            col.append(0)
+        coffmat.append(col)
+
+    all_equations = ["2x+1y+4z+1", "1x+2Y+3z+1.5", "2-1y+4x+2z"]
+
+    def CoefficientIntercept(equation):
+        if (equation.__contains__("x")) | (equation.__contains__("X")):
+            coef_x = re.findall(r'-?[0-9.]*[Xx]', equation)[0][:-1]
+
+        else:
+            coef_x = '0'
+        if (equation.__contains__("y")) | (equation.__contains__("Y")):
+            coef_y = re.findall(r'-?[0-9.]*[Yy]', equation)[0][:-1]
+        else:
+            coef_y = '0'
+        if (equation.__contains__("z")) | (equation.__contains__("Z")):
+            coef_z = re.findall(r'-?[0-9.]*[Zz]', equation)[0][:-1]
+        else:
+            coef_z = '0'
+        intercept = re.sub("[+-]?\d+[XxYyZz]|[+-]?\d+\.\d+[XxYyZz]|[+-]?\d+\.\d+[XxYyZz]", "", equation)
+        return float(coef_x), float(coef_y), float(coef_z), float(intercept)
+
+    i = 0
+    for equation in all_equations:
+        cof_x, cof_y, cof_z, cof_c, = CoefficientIntercept(equation)
+        coffmat[i][0] = cof_x
+        coffmat[i][1] = cof_y
+        coffmat[i][2] = cof_z
+        coffmat[i][3] = cof_c
+        # print(coffmat, flush=True)
+        i = i + 1
+
+    for b in range(3):
+        for l in range(b + 1, 3):
+            factor = coffmat[l][b] / coffmat[b][b]
+            for c in range(b, 3):
+                coffmat[l][c] = coffmat[l][c] - factor * coffmat[b][c]
+            coffmat[l][3] = coffmat[l][3] - factor * coffmat[b][3]
+
+
+
+    for q in range(2, -1, -1):
+        sum = 0
+        for s in range(q + 1, 3):
+            sum = sum + coffmat[q][s] * x[s]
+        x[q] = ((coffmat[q][3]) - sum) / coffmat[q][q]
+    print(x)
+
+
 
 
 
@@ -95,7 +162,7 @@ def addnum():
 
 
 
-btn1 = Button(window, text="button",bg="#338877",fg="#220050",command =gauss_jordan)
-btn1.grid(column=1, row=0)
+btn1 = Button(window, text="button",bg="#7322c7",fg="#f0f0f0",command =gauss_elimination)
+btn1.grid(column=1, row=4)
 
 window.mainloop()
